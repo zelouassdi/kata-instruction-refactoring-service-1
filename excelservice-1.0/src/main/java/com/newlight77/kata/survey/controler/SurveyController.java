9f4c675eb083555abbd18f682ca2c4163275b10f
zelouassdi <zakaria.el-ouassdi@hotmail.com>
2024-04-21 15:09:18 +0200

    SurveyController Refactoring

--- excelservice-1.0/src/main/java/com/newlight77/kata/survey/controler/SurveyController.java
+++ excelservice-1.0/src/main/java/com/newlight77/kata/survey/controler/SurveyController.java
@@ -6,6 +6,7 @@ import com.newlight77.kata.survey.service.ExportCampaignService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/survey")
 public class SurveyController {
 
     private final ExportCampaignService exportCampaignService;
@@ -13,34 +14,27 @@ public class SurveyController {
     public SurveyController(final ExportCampaignService exportCampaignService) {
       this.exportCampaignService = exportCampaignService;
     }
-
-    @RequestMapping(value = "/api/survey/create", method = RequestMethod.POST)
+    @PostMapping("/create")
     public void createSurvey(@RequestBody final Survey survey) {
         exportCampaignService.creerSurvey(survey);
     }
-
-    @RequestMapping(value = "/api/survey/get", method = RequestMethod.GET)
+    @GetMapping("/get")
     public Survey getSurvey(@RequestParam final String id) {
         return exportCampaignService.getSurvey(id);
     }
-
-    @RequestMapping(value = "/api/survey/campaign/create", method = RequestMethod.POST)
+    @PostMapping("/campaign/create")
     public void createCampaign(@RequestBody final Campaign campaign) {
         exportCampaignService.createCampaign(campaign);
     }
-
-    @RequestMapping(value = "/api/survey/campaign/get", method = RequestMethod.GET)
+    @GetMapping("/campaign/get")
     public Campaign getCampaign(@RequestParam final String id) {
         return exportCampaignService.getCampaign(id);
     }
-
-    @RequestMapping(value = "/api/survey/campaign/export", method = RequestMethod.POST)
+    @PostMapping("/campaign/export")
     public void exportCampaign(@RequestParam final String campaignId) {
-
         final Campaign campaign = exportCampaignService.getCampaign(campaignId);
         final Survey survey = exportCampaignService.getSurvey(campaign.getSurveyId());
         exportCampaignService.sendResults(campaign, survey);
-        
     }
 }
 
